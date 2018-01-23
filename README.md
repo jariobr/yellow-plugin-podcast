83be0dde44c5ac324f9493aa9e10ed21fbc60de8
Steffen Schultz <steffenschultz@mailbox.org>
2018-01-23 12:45:50 +0100

    Plugin release

---
+++ README.md
@@ -0,0 +1,56 @@
+Podcast plugin 0.7.1
+====================
+Simple podcast feed for media files. Based on the [original Yellow feed plugin](https://github.com/datenstrom/yellow-plugins/tree/master/feed). 
+
+## How do I install this?
+
+1. [Download and install Datenstrom Yellow](https://github.com/datenstrom/yellow/).
+2. [Download plugin](https://github.com/schulle4u/yellow-plugin-podcast/archive/master.zip). If you are using Safari, right click and select 'Download file as'.
+3. Copy `podcast.zip` into your `system/plugins` folder.
+
+To uninstall delete the [plugin files](update.ini).
+
+## How to use a podcast?
+
+The podcast feed is available on your website as `http://website/podcast/` and `http://website/podcast/page:podcast.xml`. It's a feed for the entire website, only visible pages are included. To make a blog podcast feed open file `system/config/config.ini` and change `PodcastFilter: blog`. You can add a link to the podcast somewhere on your website. 
+
+## How to configure a podcast?
+
+The following settings can be configured in file `system/config/config.ini`:
+
+`PodcastLocation` = podcast location  
+`PodcastFileXml` = feed file name for RSS podcast feed  
+`PodcastFilter` = podcast template filter  
+`PodcastPaginationLimit` = number of entries to show per page  
+`PodcastMimeType` = mimetype of included files, default is audio/mpeg (mp3)  
+
+To enable media file support in the feed, you additionally have to define the file in the page metadata, see example below. 
+
+## Example
+
+Here is a blog page with included media content. 
+
+```
+---
+Title: A new podcast episode
+Published: 2018-01-01 12:45:00
+Author: Steffen
+Template: blog
+Mediafile: http://cdn.yourdomain.tld/podcast/files/episode1.mp3
+---
+This is the article for your podcast episode. It is recommended to use a page break to provide your listeners with a summary for their podcatchers. Furthermore you should link the media file [somewhere in this text](http://cdn.yourdomain.tld/podcast/files/episode1.mp3) for direct download. 
+```
+
+## Todo
+
+* iTunes metadata
+* Dedicated podcast page template
+
+
+## Developer
+
+Datenstrom with modifications by Steffen Schultz
+
+## See also
+
+* [Audio plugin](https://github.com/schulle4u/yellow-plugin-audio/), useful to directly play the podcast on your website. 
---
+++ podcast.php
@@ -0,0 +1,93 @@
+<?php
+// Podcast Plugin
+// Based on the yellow Feed plugin, https://github.com/datenstrom/yellow-plugins/tree/master/feed
+// Copyright (c) 2013-2017 Datenstrom, https://datenstrom.se
+// This file may be used and distributed under the terms of the public license.
+
+class YellowPodcast
+{
+	const VERSION = "0.7.1";
+	var $yellow;			//access to API
+	
+	// Handle initialisation
+	function onLoad($yellow)
+	{
+		$this->yellow = $yellow;
+		$this->yellow->config->setDefault("podcastLocation", "/podcast/");
+		$this->yellow->config->setDefault("podcastFileXml", "podcast.xml");
+		$this->yellow->config->setDefault("podcastFilter", "");
+		$this->yellow->config->setDefault("podcastPaginationLimit", "30");
+		$this->yellow->config->setDefault("podcastMimeType", "audio/mpeg");
+	}
+
+	// Handle page parsing
+	function onParsePage()
+	{
+		if($this->yellow->page->get("template")=="podcast")
+		{
+			$podcastFilter = $this->yellow->config->get("podcastFilter");
+			$chronologicalOrder = ($this->yellow->config->get("podcastFilter")!="blog");
+			$pagination = $this->yellow->config->get("contentPagination");
+			if($_REQUEST[$pagination]==$this->yellow->config->get("podcastFileXml"))
+			{
+				$pages = $this->yellow->pages->index(false, false);
+				if(!empty($podcastFilter)) $pages->filter("template", $podcastFilter);
+				$pages->sort($chronologicalOrder ? "modified" : "published", false);
+				$pages->limit($this->yellow->config->get("podcastPaginationLimit"));
+				$this->yellow->page->setLastModified($pages->getModified());
+				$this->yellow->page->setHeader("Content-Type", "application/rss+xml; charset=utf-8");
+				$output = "<?xml version=\"1.0\" encoding=\"utf-8\"\077>\r\n";
+				$output .= "<rss version=\"2.0\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\r\n";
+				$output .= "<channel>\r\n";
+				$output .= "<title>".$this->yellow->page->getHtml("sitename")."</title>\r\n";
+				$output .= "<link>".$this->yellow->page->scheme."://".$this->yellow->page->address.$this->yellow->page->base."/"."</link>\r\n";
+				$output .= "<description>".$this->yellow->page->getHtml("tagline")."</description>\r\n";
+				$output .= "<language>".$this->yellow->page->getHtml("language")."</language>\r\n";
+				foreach($pages as $page)
+				{
+					$timestamp = strtotime($page->get($chronologicalOrder ? "modified" : "published"));
+					$content = $this->yellow->toolbox->createTextDescription($page->getContent(), 0, false, "<!--more-->", " <a href=\"".$page->getUrl()."\">".$this->yellow->text->getHtml("blogMore")."</a>");
+					$output .= "<item>\r\n";
+					$output .= "<title>".$page->getHtml("title")."</title>\r\n";
+					$output .= "<link>".$page->getUrl()."</link>\r\n";
+					$output .= "<pubDate>".date(DATE_RSS, $timestamp)."</pubDate>\r\n";
+					$output .= "<guid isPermaLink=\"false\">".$page->getUrl()."?".$timestamp."</guid>\r\n";
+					$output .= "<dc:creator>".$page->getHtml("author")."</dc:creator>\r\n";
+					$output .= "<description>".$page->getHtml("description")."</description>\r\n";
+					$output .= "<content:encoded><![CDATA[".$content."]]></content:encoded>\r\n";
+					if($this->yellow->page->isExisting("mediafile")) $output .= "<enclosure url=\"".$this->yellow->page->getHtml("mediafile")."\" length=\"0\" type=\"".$this->yellow->config->get("podcastMimeType")."\" />\r\n";
+					$output .= "</item>\r\n";
+				}
+				$output .= "</channel>\r\n";
+				$output .= "</rss>\r\n";
+				$this->yellow->page->setOutput($output);
+			} else {
+				$pages = $this->yellow->pages->index(false, false);
+				if(!empty($podcastFilter)) $pages->filter("template", $podcastFilter);
+				$pages->sort($chronologicalOrder ? "modified" : "published");
+				$pages->pagination($this->yellow->config->get("podcastPaginationLimit"));
+				if(!$pages->getPaginationNumber()) $this->yellow->page->error(404);
+				$this->yellow->page->set("podcastChronologicalOrder", $chronologicalOrder);
+				$this->yellow->page->setPages($pages);
+				$this->yellow->page->setLastModified($pages->getModified());
+			}
+		}
+	}
+	
+	// Handle page extra HTML data
+	function onExtra($name)
+	{
+		$output = NULL;
+		if($name=="header")
+		{
+			$pagination = $this->yellow->config->get("contentPagination");			
+			$locationPodcast = $this->yellow->config->get("serverBase").$this->yellow->config->get("podcastLocation");
+			$locationPodcast .= $this->yellow->toolbox->normaliseArgs("$pagination:".$this->yellow->config->get("podcastFileXml"), false);
+			$output = "<link rel=\"alternate\" type=\"application/rss+xml\" href=\"$locationPodcast\" />\n";
+		}
+		return $output;
+	}
+}
+
+$yellow->plugins->register("podcast", "YellowPodcast", YellowPodcast::VERSION);
+?>
