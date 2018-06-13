90bf8a70d276b37c791abb912e431886146374ec
Steffen Schultz <steffenschultz@mailbox.org>
2018-06-13 21:08:21 +0200

    Title by tag

--- content-podcast.php
+++ content-podcast.php
@@ -1,7 +1,7 @@
 <div class="content">
 <?php $yellow->snippet("sidebar") ?>
 <div class="main">
-<h1><?php echo $yellow->page->getHtml("title") ?> - <?php echo $this->yellow->text->getHtml("PodcastArchive") ?></h1>
+<h1><?php if($_REQUEST["tag"]) { echo ucfirst($_REQUEST["tag"]); } else { echo $yellow->page->getHtml("title"); } ?> - <?php echo $this->yellow->text->getHtml("PodcastArchive") ?></h1>
 <ul>
 <?php foreach($yellow->page->getPages() as $page): ?>
 <?php if($yellow->page->get("podcastChronologicalOrder")): ?>
